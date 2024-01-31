3265d1661cc1615e58d0ae6f093fa687ffdd4eb4
MegaBluejay <dmoiseev2011@gmail.com>
2024-01-31 12:06:58 +0700

    add icon and name, url params

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use atom_syndication::{
     Entry, EntryBuilder, Feed, FeedBuilder, FixedDateTime, LinkBuilder, PersonBuilder, Text,
 };
@@ -18,21 +18,14 @@ pub async fn main(
     let router = Router::new();
 
     router
-        .get_async("/feed.xml", |_req, _ctx| async move {
-            let params = KemonoFeed {
-                service: "patreon".to_owned(),
-                user_id: "93878".to_owned(),
-                username: "malinryden".to_owned(),
+        .get_async("/:service/:user_id/feed.xml", |_req, ctx| async move {
+            let feed = KemonoFeed {
+                service: ctx.param("service").unwrap().as_ref(),
+                user_id: ctx.param("user_id").unwrap().as_ref(),
             };
 
-            let posts = get_kemono_posts(&params)
+            let out = render_feed(feed)
                 .await
-                .map_err(|e| worker::Error::RustError(e.to_string()))?;
-
-            let feed = kemono_feed(&params, posts);
-
-            let out = feed
-                .write_to(vec![])
                 .map_err(|e| worker::Error::RustError(e.to_string()))?;
 
             let mut headers = worker::Headers::new();
@@ -44,10 +37,17 @@ pub async fn main(
         .await
 }
 
-pub struct KemonoFeed {
-    pub service: String,
-    pub user_id: String,
-    pub username: String,
+pub async fn render_feed(feed: KemonoFeed<'_>) -> Result<Vec<u8>> {
+    let posts = get_kemono_posts(feed).await?;
+    let atom_feed = kemono_feed(feed, posts).await?;
+    let out = atom_feed.write_to(vec![])?;
+    Ok(out)
+}
+
+#[derive(Clone, Copy)]
+pub struct KemonoFeed<'a> {
+    pub service: &'a str,
+    pub user_id: &'a str,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -58,12 +58,15 @@ pub struct KemonoPost {
     pub published: NaiveDateTime,
 }
 
-pub fn kemono_entry(feed: &KemonoFeed, post: KemonoPost) -> Entry {
+static BASE_URL: &str = "https://kemono.su";
+static BASE_IMG_URL: &str = "https://img.kemono.su";
+
+pub fn kemono_entry(feed: KemonoFeed<'_>, post: KemonoPost) -> Entry {
     let dtm = post.published.and_utc();
 
     let link = format!(
-        "https://kemono.su/{}/user/{}/post/{}",
-        &feed.service, &feed.user_id, &post.id
+        "{}/{}/user/{}/post/{}",
+        BASE_URL, feed.service, feed.user_id, &post.id
     );
 
     let summary = Html::parse_fragment(&post.content)
@@ -83,43 +86,61 @@ pub fn kemono_entry(feed: &KemonoFeed, post: KemonoPost) -> Entry {
         .build()
 }
 
-pub fn kemono_feed(feed: &KemonoFeed, posts: impl IntoIterator<Item = KemonoPost>) -> Feed {
+pub async fn kemono_feed(
+    feed: KemonoFeed<'_>,
+    posts: impl IntoIterator<Item = KemonoPost>,
+) -> Result<Feed> {
     let entries = posts
         .into_iter()
         .map(|post| kemono_entry(feed, post))
         .collect::<Vec<_>>();
+
+    let name = get_kemono_name(feed).await?;
 
     let updated = entries
         .first()
         .map(|entry| entry.updated)
         .unwrap_or(FixedDateTime::from_str("1970-01-01T00:00:00Z").unwrap());
 
-    let link = format!("https://kemono.su/{}/user/{}", &feed.service, &feed.user_id);
+    let icon = format!("{}/icons/{}/{}", BASE_IMG_URL, feed.service, feed.user_id);
 
-    FeedBuilder::default()
-        .id(format!("kemono-feed/{}/{}", &feed.service, &feed.user_id))
-        .title(format!(
-            "Posts of {} from {}",
-            &feed.username, &feed.service
-        ))
+    Ok(FeedBuilder::default()
+        .id(format!("kemono-feed/{}/{}", feed.service, feed.user_id))
+        .title(format!("Posts of {} from {}", name, feed.service))
         .updated(updated)
-        .authors(vec![PersonBuilder::default()
-            .name(feed.username.clone())
-            .build()])
+        .authors(vec![PersonBuilder::default().name(name).build()])
         .links(vec![LinkBuilder::default()
-            .href(link)
+            .href(kemono_link(feed))
             .rel("alternate".to_owned())
             .build()])
+        .icon(Some(icon))
         .entries(entries)
-        .build()
+        .build())
 }
 
-pub async fn get_kemono_posts(feed: &KemonoFeed) -> Result<Vec<KemonoPost>> {
+pub async fn get_kemono_posts(feed: KemonoFeed<'_>) -> Result<Vec<KemonoPost>> {
     Ok(reqwest::get(format!(
-        "https://kemono.su/api/v1/{}/user/{}",
-        &feed.service, &feed.user_id
+        "{}/api/v1/{}/user/{}",
+        BASE_URL, feed.service, feed.user_id
     ))
     .await?
     .json()
     .await?)
+}
+
+fn kemono_link(feed: KemonoFeed) -> String {
+    format!("{}/{}/user/{}", BASE_URL, feed.service, feed.user_id)
+}
+
+pub async fn get_kemono_name(feed: KemonoFeed<'_>) -> Result<String> {
+    let page = reqwest::get(kemono_link(feed)).await?.text().await?;
+
+    let html = Html::parse_document(&page);
+
+    let name_span = html
+        .select(&Selector::parse("span[itemprop=name]").unwrap())
+        .next()
+        .context("failed to find name")?;
+
+    Ok(name_span.inner_html())
 }
